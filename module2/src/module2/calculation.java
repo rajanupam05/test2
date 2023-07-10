d86e276f2ae3c70338babdfdd1f4d011f70793a4
Acer <Acer@LAPTOP-6C57R8LE>
2023-07-10 15:43:20 +0530

    module 2 for collect imported jar file from module 1

---
+++ module2/src/module2/calculation.java
@@ -0,0 +1,13 @@
+package module2;
+
+import calculator.class1;
+
+public class calculation 
+{
+	public static void main(String[] args) {
+		class1 abc=new class1();
+		abc.add(10, 12);
+		abc.subtract(10, 12);
+		abc.multiply(5,10);
+	}
+}
---
+++ module2/src/salary_system/salary_system.java
@@ -0,0 +1,12 @@
+package salary_system;
+
+import salary.salary;
+
+public class salary_system 
+{
+	public static void main(String[] args) {
+		salary abc=new salary();
+		abc.m1(60);
+		abc.m2(20);
+	}
+}
